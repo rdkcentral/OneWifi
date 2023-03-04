94b77974078ea98bfecf19f7714bc9d199c5a73f
Jonathan Wu <jonathan_wu@comcast.com>
2023-03-04 00:10:36 +0000

    RDKB-47676 : Add self heal support for 6G
    Reason for change: Modify OneWiFi_Selfheal.sh to monitor 6G radio
    Test Procedure: - Use 'wl -i wl2.1 bss down' to bring down private VAP
                    - Can also bring down wl0.1 and wl1.1 for testing
                    - Wait for a maximum of 15 minutes
                    - All private VAP should be up
    Risks: None
    Priority: P1
    
    Change-Id: I0b8e9ff9fb7b478f55f782a8950a344b920a4db0
    Signed-off-by: Jonathan Wu <Jonathan_Wu@comcast.com>

--- scripts/OneWiFi_Selfheal.sh
+++ scripts/OneWiFi_Selfheal.sh
@@ -4,40 +4,34 @@ source /lib/rdk/t2Shared_api.sh
 check_count=0
 vap_2g_down=0
 vap_5g_down=0
+vap_6g_down=0
 pre_timestamp=0
 cur_timestamp=0
+private_2g_instance=1
+private_5g_instance=2
+private_6g_instance=17
+
+MODEL_NUM=`grep MODEL_NUM /etc/device.properties | cut -d "=" -f2`
+LOG_FILE="/rdklogs/logs/wifi_selfheal.txt"
 
 onewifi_restart_wifi()
 {
-        echo_t "private_vap is down self heal is executing" >>  /rdklogs/logs/wifi_selfheal.txt
+        echo_t "private_vap is down self heal is executing" >> $LOG_FILE
         systemctl restart onewifi.service
-        echo_t "private_vap self heal executed onewifi restarted" >>  /rdklogs/logs/wifi_selfheal.txt
+        echo_t "private_vap self heal executed onewifi restarted" >> $LOG_FILE
 }
 
-private_vap_2g_restart()
+vap_restart()
 {
-        echo_t "private_2g is down self heal is executing" >>  /rdklogs/logs/wifi_selfheal.txt
-        max_sta_2g=`dmcli eRT getv Device.WiFi.AccessPoint.1.X_CISCO_COM_BssMaxNumSta | grep "value:" | cut -f2- -d:| cut -f2- -d:`
-        if [ $max_sta_2g == 75 ]; then
-                dmcli eRT setv Device.WiFi.AccessPoint.1.X_CISCO_COM_BssMaxNumSta int 74
-        else
-                dmcli eRT setv Device.WiFi.AccessPoint.1.X_CISCO_COM_BssMaxNumSta int 75
-        fi
-        dmcli eRT setv Device.WiFi.ApplyAccessPointSettings bool true
-        echo_t "private_2g self heal executed" >>  /rdklogs/logs/wifi_selfheal.txt
-}
-
-private_vap_5g_restart()
-{
-        echo_t "private_5g is down self heal is executing" >>  /rdklogs/logs/wifi_selfheal.txt
-        max_sta_5g=`dmcli eRT getv Device.WiFi.AccessPoint.2.X_CISCO_COM_BssMaxNumSta | grep "value:" | cut -f2- -d:| cut -f2- -d:`
-        if [ $max_sta_5g == 75 ]; then
-                dmcli eRT setv Device.WiFi.AccessPoint.2.X_CISCO_COM_BssMaxNumSta int 74
-        else
-                dmcli eRT setv Device.WiFi.AccessPoint.2.X_CISCO_COM_BssMaxNumSta int 75
-        fi
-        dmcli eRT setv Device.WiFi.ApplyAccessPointSettings bool true
-        echo_t "private_5g  self heal executed" >>  /rdklogs/logs/wifi_selfheal.txt
+    echo_t "$1 is down. Self heal is executing" >> $LOG_FILE
+    max_sta=`dmcli eRT getv Device.WiFi.AccessPoint.$2.X_CISCO_COM_BssMaxNumSta | grep "value:" | cut -f2- -d:| cut -f2- -d:`
+    if [ $max_sta == 75 ]; then
+        dmcli eRT setv Device.WiFi.AccessPoint.$2.X_CISCO_COM_BssMaxNumSta int 74 > /dev/null
+    else
+        dmcli eRT setv Device.WiFi.AccessPoint.$2.X_CISCO_COM_BssMaxNumSta int 75 > /dev/null
+    fi
+    dmcli eRT setv Device.WiFi.ApplyAccessPointSettings bool true > /dev/null
+    echo_t "$1  self heal executed" >> $LOG_FILE
 }
 
 while true
@@ -45,24 +39,24 @@ while true
  if [ $check_count == 3 ]; then
         check_count=0
         cur_timestamp="`date +"%s"` $1"
-        #echo_t "cur_timestamp = $cur_timestamp" >> /rdklogs/logs/wifi_selfheal.txt
+        #echo_t "cur_timestamp = $cur_timestamp" >> $LOG_FILE
         status_2g=`dmcli eRT getv Device.WiFi.AccessPoint.1.Enable | grep "value:" | cut -f2- -d:| cut -f2- -d:`
          if [ $status_2g == "true" ]; then
                 ssid_2g=`wl -i wl0.1 status | grep  -m 1 "BSSID:" | cut -d ":" -f2-7 | awk '{print $1}'`
                 if [ $ssid_2g ==  "00:00:00:00:00:00" ];then
                         if [ $vap_2g_down == 1 ]; then
                                 time_diff=`expr $cur_timestamp - $pre_timestamp`
-                                echo_t "time_diff = $time_diff" >> /rdklogs/logs/wifi_selfheal.txt
+                                echo_t "time_diff = $time_diff" >> $LOG_FILE
                                 if [ $time_diff -ge 43200 ]; then
                                         onewifi_restart_wifi
                                         pre_timestamp="`date +"%s"` $1"
                                         vap_2g_down=0
                                         continue
                                 else
-                                        private_vap_2g_restart
+                                        vap_restart "private_2g" $private_2g_instance
                                 fi
                         else
-                                private_vap_2g_restart
+                                vap_restart "private_2g" $private_2g_instance
                                 vap_2g_down=1
                         fi
                 else
@@ -75,24 +69,50 @@ while true
                 if [ $ssid_5g ==  "00:00:00:00:00:00" ];then
                         if [ $vap_5g_down == 1 ]; then
                                 time_diff=`expr $cur_timestamp - $pre_timestamp`
-                                echo_t "time_diff = $time_diff" >> /rdklogs/logs/wifi_selfheal.txt
+                                echo_t "time_diff = $time_diff" >> $LOG_FILE 
                                 if [ $time_diff -ge 43200 ]; then
                                         onewifi_restart_wifi
                                         pre_timestamp="`date +"%s"` $1"
                                         vap_5g_down=0
                                         continue
                                 else
-                                        private_vap_5g_restart
+                                        vap_restart "private_5g" $private_5g_instance
                                 fi
                         else
-                                private_vap_5g_restart
+                                vap_restart "private_5g" $private_5g_instance
                                 vap_5g_down=1
                         fi
                 else
                         vap_5g_down=0
                 fi
         fi
+
+        if [ "$MODEL_NUM" == "CGM4981COM" ]; then
+            status_6g=`dmcli eRT getv Device.WiFi.AccessPoint.$private_6g_instance.Enable | grep "value:" | cut -f2- -d:| cut -f2- -d:`
+            if [ $status_6g == "true" ]; then
+                bss_status="`wl -i wl2.1 bss`"
+                if [ $bss_status == "down" ]; then
+                    if [ $vap_6g_down == 1 ]; then
+                        time_diff=`expr $cur_timestamp - $pre_timestamp`
+                        echo_t "time_diff = $time_diff" >> $LOG_FILE
+                        if [ $time_diff -ge 43200 ]; then
+                            onewifi_restart_wifi
+                            pre_timestamp="`date +"%s"` $1"
+                            vap_6g_down=0
+                            continue
+                        else
+                            vap_restart "private_6g" $private_6g_instance
+                        fi
+                    else
+                        vap_restart "private_6g" $private_6g_instance
+                        vap_6g_down=1
+                    fi
+                else
+                    vap_6g_down=0
+                fi
+            fi
+        fi
  fi
  sleep 5m
-((check_count++))
+ ((check_count++))
 done
