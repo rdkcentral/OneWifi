a70dc06faed6f7b670a44d539af8dbc93a24e64d
rchand202 <rahul_chandranr@comcast.com>
2023-05-12 18:24:37 +0000

    BTEX-2177: Enable DFS for pods
    Reason for change: DFS channels are not updated in Radio state
    Test Procedure: Verify Radio State is updated with DFS channels with proper state
    Risks: medium
    
    Change-Id: I745a1c75199100f233417cb24fe380defa11af7e
    Signed-off-by: rchand202 <rahul_chandranr@comcast.com>
    (cherry picked from commit 09bba93bcb86a6a915a939e4cda1ff1114e2cb16)

--- source/db/wifi_db.c
+++ source/db/wifi_db.c
@@ -3722,6 +3722,7 @@ int wifidb_init_radio_config_default(int radio_index,wifi_radio_operationParam_t
 #if defined (_PP203X_PRODUCT_REQ_)
             cfg.variant = WIFI_80211_VARIANT_A | WIFI_80211_VARIANT_N | WIFI_80211_VARIANT_AC;
             cfg.beaconInterval = 200;
+            cfg.DfsEnabled = true;
 #else
             cfg.variant = WIFI_80211_VARIANT_A | WIFI_80211_VARIANT_N | WIFI_80211_VARIANT_AC | WIFI_80211_VARIANT_AX;
 #endif
