e8c31dfa911f9fd23f93806453429bca59b67b20
Yanina Husarevych <yanina_husarevych@comcast.com>
2023-07-07 13:09:06 +0300

    CMXB7-5185: bring vaps down when device is in LPM
    Reason for change: bring vaps down when device operates in
    Low Power Mode.
    Test Procedure: Enable LPM by pulling the plug or using
    /bin/sh /usr/ccsp/pwrMgr/rdkb_power_manager.sh POWER_TRANS_BATTERY
    Verify whether OneWifi service is stopped and connected clients are
    disconnected and vaps down.
    Transition back to AC mode by plugging in the power or using
    /bin/sh /usr/ccsp/pwrMgr/rdkb_power_manager.sh POWER_TRANS_AC
    Verify whether OneWifi service is started and able to connect clients and
    vaps are up without any issues.
    Risks: low
    Priority: P1
    
    Change-Id: I6381e943c870ccd077e334a2c34eb969c38a3d3a
    Signed-off-by: Yanina Husarevych <yanina_husarevych@comcast.com>
    (cherry picked from commit 5ba8e0d290f1f8d74b9219d2f9c85c7f5f184e98)

--- scripts/OneWiFi_vap_down.sh
+++ scripts/OneWiFi_vap_down.sh
@@ -25,3 +25,20 @@ if [ "$MODEL_NUM" == "CGM4981COM" ]; then
         echo "third radio vaps are going to down state"
     done
 fi
+
+if [ "$MODEL_NUM" == "TG4482A" ]; then
+    for i in 0 2
+    do
+        for j in 0 1 2 3 4 5 6
+        do
+            ifconfig wlan"$i"."$j" down
+        done
+    done
+
+    for i in 1 3
+    do
+        ifconfig wlan"$i" down
+    done
+
+    echo "all vaps are going to down state..." >> $LOG_FILE
+fi
