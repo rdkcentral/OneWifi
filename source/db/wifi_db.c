71c2b5496abfea01b27150db0d354d1f645d623b
Harshavardhan Pulluru <Harshavardhan_Pulluru@Comcast.com>
2023-08-02 11:14:05 +0000

    RDKB-50825 : [ONEWIFI] STBC def mode is false, after enab also in beacon it is 0
    Reason for change:
       - [ONEWIFI] STBC default mode is false,
         after enable also in beacon it is 0
    Impacted Platforms:
    RDKB
    Test Procedure:
    After Factory reset ,STBC should be seen enabled in dmcli and in beacon.
    If STBC is disabled by dmcli then it should be seen as disabled in both
    dmcli and beacon.
    Risks: low
    Priority: P2
    Signed-off-by:Harshavardhan_Pulluru@comcast.com
    
    Change-Id: Iae9eeac0c74cce9180b2eee9aea80c7d7567effc

--- source/db/wifi_db.c
+++ source/db/wifi_db.c
@@ -4747,7 +4747,7 @@ int wifidb_init_radio_config_default(int radio_index,wifi_radio_operationParam_t
     cfg.guardInterval = wifi_guard_interval_auto;
     cfg.ctsProtection = false;
     cfg.obssCoex = true;
-    cfg.stbcEnable = false;
+    cfg.stbcEnable = true;
     cfg.greenFieldEnable = false;
     cfg.userControl = 0;
     cfg.adminControl = 0;
