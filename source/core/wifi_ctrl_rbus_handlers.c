a0cb22831341a9818687b0121735dee596669bc7
Amarnath Hullur Subramanyam <182549733+amarnathhullur@users.noreply.github.com>
2024-12-13 09:52:00 -0800

    Merge pull request #51 from rdkcentral/MultiVAP_Subdoc_changes
    Multi vap, Single Radio subdoc changes and Easymesh translation changes
    
    (cherry picked from commit 103e9f8c8ba585b4b6596aa1eec30f7cd12f7535)

--- source/core/wifi_ctrl_rbus_handlers.c
+++ source/core/wifi_ctrl_rbus_handlers.c
@@ -840,9 +840,6 @@ int webconfig_bus_apply(wifi_ctrl_t *ctrl, webconfig_subdoc_encoded_data_t *data
     wifi_util_dbg_print(WIFI_CTRL, "%s:%d:bus_event_publish_fn WIFI_WEBCONFIG_DOC_DATA_NORTH initiated\n", __func__,
             __LINE__);
 
-    wifi_util_dbg_print(WIFI_CTRL, "%s:%d:bus_event_publish_fn WIFI_WEBCONFIG_DOC_DATA_NORTH initiated %d\n", __func__,
-            __LINE__);
-
     rc = get_bus_descriptor()->bus_event_publish_fn(&ctrl->handle, WIFI_WEBCONFIG_DOC_DATA_NORTH,
         &rdata);
     if (rc != bus_error_success) {
