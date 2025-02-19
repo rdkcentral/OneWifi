c21865148e49c19175cd18ccec2175d079fd634b
LakshminarayananShenbagaraj <lakshminarayanan.shenbagaraj2@sky.uk>
2025-02-19 17:19:51 +0000

    XER10-905 : Multiple logs are flooding.
    Reason for change:
    As UK version of XER10 doesn't support GFO(Gateway Fail Over) feature so GatewayManager process doesn't available. But we have seen GatewayManager related TR181 parameter requesting for subscription from OneWiFi/WiFiAgent/MeshAgent components. so we have introduced Device.X_RDK_Features.GatewayFailover.Enable TR181 for consumer.
    
    Test Procedure:
    1. PAM should work without any issue.
    2. dmcli eRT getv Device.X_RDK_Features.GatewayFailover.Enable should provide result according to product support.
    
    Risks: Low
    
    Signed-off-by: LakshminarayananShenbagaraj <lakshminarayanan.shenbagaraj2@sky.uk>

--- source/core/wifi_ctrl.h
+++ source/core/wifi_ctrl.h
@@ -93,9 +93,7 @@ extern "C" {
 
 #define ETH_BH_STATUS                      "Device.X_RDK_MeshAgent.EthernetBhaulUplink.Status"
 
-#if defined(_RDKB_GLOBAL_PRODUCT_REQ_)
-#define TR181_GLOBAL_FEATURE_PARAM_GFO_SUPPORTED    "Device.X_RDK_Features.GatewayFailover.Enable"
-#endif
+#define TR181_GLOBAL_FEATURE_PARAM_GFO_SUPPORTED "Device.X_RDK_Features.GatewayFailover.Enable"
 
 #define WIFI_ALL_RADIO_INDICES             0xffff
 #define DEVICE_TUNNEL_UP                   1
