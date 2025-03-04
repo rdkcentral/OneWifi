103ce3953eb907a1e9c30b2753a8e885fef16fb6
AmaleshNandh29 <amalesh_nandh@comcast.com>
2025-03-04 18:13:25 +0530

    Update wifi_ctrl_queue_handlers.c

--- source/core/wifi_ctrl_queue_handlers.c
+++ source/core/wifi_ctrl_queue_handlers.c
@@ -3083,7 +3083,7 @@ void process_rsn_override_rfc(bool type)
         }
 
         if (radio_params->band == WIFI_FREQUENCY_6_BAND) {
-            wifi_util_info_print(WIFI_DB,"%s: %d 6GHz radio supports only WPA3 personal mode. WPA3-RFC: %d\n",__FUNCTION__,__LINE__,type);
+            wifi_util_info_print(WIFI_CTRL,"%s: %d 6GHz radio supports only WPA3 personal mode. WPA3-RFC: %d\n",__FUNCTION__,__LINE__,type);
             continue;
         }
 
@@ -3127,9 +3127,9 @@ void process_rsn_override_rfc(bool type)
         apps_mgr_analytics_event(&ctrl->apps_mgr, wifi_event_type_webconfig, wifi_event_webconfig_hal_result, update_status);
 
         if (ret != RETURN_OK) {
-            wifi_util_error_print(WIFI_DB,"%s:%d: Private vaps service update_fn failed \n",__func__, __LINE__);
+            wifi_util_error_print(WIFI_CTRL,"%s:%d: Private vaps service update_fn failed \n",__func__, __LINE__);
         } else {
-            wifi_util_dbg_print(WIFI_DB,"%s:%d: Updating security mode for apIndex %d secmode %d \n",__func__, __LINE__,apIndex,vapInfo->u.bss_info.security.mode);
+            wifi_util_dbg_print(WIFI_CTRL,"%s:%d: Updating security mode for apIndex %d secmode %d \n",__func__, __LINE__,apIndex,vapInfo->u.bss_info.security.mode);
         }
     }
 }
