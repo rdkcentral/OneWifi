8a6c97f9fef0d260946fe1e3cdb1a6a75f5b7eee
Jayanthrajan <jayanth.rajan@sky.uk>
2025-03-28 16:32:52 +0000

    RDKB-59277 : Initial commit for Station Manager App. (#234)
    Reason for change: Introducing code for station Manager app.
    Test Procedure: OneWIfi process should build and run properly.
    Risks: Low
    Priority:P1
    Cherrypicked from commit ad7bd5bdd65e5a16342ec272205a198f4501e87f

--- source/apps/wifi_apps.c
+++ source/apps/wifi_apps.c
@@ -262,6 +262,12 @@ int em_event(wifi_app_t *app, wifi_event_t *event)
 }
 #endif // EM_APP
 
+#ifdef ONEWIFI_STA_MGR_APP_SUPPORT
+extern int sta_mgr_init(wifi_app_t *app, unsigned int create_flag);
+extern int sta_mgr_deinit(wifi_app_t *app);
+extern int sta_mgr_event(wifi_app_t *app, wifi_event_t *event);
+#endif
+
 wifi_app_descriptor_t app_desc[] = {
 #ifdef ONEWIFI_ANALYTICS_APP_SUPPORT
     {
