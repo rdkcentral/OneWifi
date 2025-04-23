175d1229fe4afdb2c1139876dbc6496b8c07584b
Narendra Varma Dandu <narendandu@gmail.com>
2025-04-23 10:40:56 -0700

    Update wifi_db_apis.c

--- source/db/wifi_db_apis.c
+++ source/db/wifi_db_apis.c
@@ -6334,7 +6334,7 @@ int wifidb_init_radio_config_default(int radio_index,wifi_radio_operationParam_t
 #ifdef NEWPLATFORM_PORT
             cfg.variant |= WIFI_80211_VARIANT_AX;
 #endif /* NEWPLATFORM_PORT */
-#ifdef CONFIG_IEEE80211BE
+#if defined(_PLATFORM_BANANAPI_R4_) && defined(CONFIG_IEEE80211BE)
             cfg.variant |= WIFI_80211_VARIANT_BE;
 #endif /* CONFIG_IEEE80211BE */
 #if defined (_PP203X_PRODUCT_REQ_)
