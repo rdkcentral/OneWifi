015fdf4eebe96f0dcf4995324c3a7ff55add1430
mmalla642 <mothishree_mallaiyanjothimani@comcast.com>
2025-04-26 14:48:31 +0000

    RDKB-36610: Debug
    Reason for change: for onboarding
    Test Procedure: Debug
    Priority: P0
    Risks: Low
    Signed-off-by: mothishree_mallaiyanjothimani@comcast.com

--- source/apps/sm/sm_utils.c
+++ source/apps/sm/sm_utils.c
@@ -85,6 +85,7 @@ static const str_to_dpp_chan_width_t chan_width_mapping[] = {
     { "80",              RADIO_CHAN_WIDTH_80MHZ},
     { "40",              RADIO_CHAN_WIDTH_40MHZ},
     { "20",              RADIO_CHAN_WIDTH_20MHZ},
+    { "320MHz",          RADIO_CHAN_WIDTH_320MHZ},
 };
 
 
