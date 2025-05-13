432323b3e67ad7f227140d8ae10fbe2a28017d5a
Amarnath Hullur Subramanyam <amarnath_hullursubramanyam@comcast.com>
2025-05-13 14:08:27 -0700

    RDKB-59404: OpenWRT related onewifi configuration
    Few files which are required for onewifi execution on OpenWRT
    1)InterfaceMap.json to depict the initial interfaces and mapping.
    2)/etc/init.d/onewifi systemd related file
    3)log_enablement.sh to enable certain important logging
    4)wifi_interface_up.sh to bring up wifi interface with associated
    EasymeshCfg.json changes.
    5)wifi_interface_down.sh to bring down wifi interfaces and cleanup.

---
+++ config/openwrt/onewifi_bpi_config/log_enablement.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+mkdir -p /nvram
+chmod 777 /nvram
+touch /nvram/wifiWebConfigDbg
+touch /nvram/wifiHalDbg
+touch /nvram/wifiMgrDbg
+touch /nvram/wifiCtrlDbg
+touch /nvram/wifiHalStatsDbg
+
+
+chmod 777 /nvram/wifiWebConfigDbg
+chmod 777 /nvram/wifiHalDbg
+chmod 777 /nvram/wifiMgrDbg
+chmod 777 /nvram/wifiCtrlDbg
+chmod 777 /nvram/wifiHalStatsDbg
