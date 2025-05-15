f559050a9eac4aaec9ff7c0e5a02513c9189aed6
mmalla642 <mothishree_mallaiyanjothimani@comcast.com>
2025-05-15 11:28:26 +0000

    RDKB-36610: Debug
    Reason for change: for onboarding
    Test Procedure: Debug
    Priority: P0
    Risks: Low
    Signed-off-by: mothishree_mallaiyanjothimani@comcast.com

--- build/linux/setup.sh
+++ build/linux/setup.sh
@@ -8,7 +8,7 @@ SRCREV_2_10="9d07b9447e76059a2ddef2a879c57d0934634188"
 #git clone other wifi related components
 cd ..
 git clone https://github.com/rdkcentral/rdk-wifi-hal.git rdk-wifi-hal
-git clone https://github.com/Aniket0606/rdkb-halif-wifi.git halinterface
+git clone https://github.com/rdkcentral/rdkb-halif-wifi.git halinterface
 cd $ONEWIFI_DIR
 mkdir -p install/bin
 mkdir -p install/lib
@@ -34,7 +34,8 @@ git clone https://github.com/rdkcentral/hostap-patches.git hostap-patches
 
 #Apply the patch
 patch_filenames="hostap-patches/0001-OneWifi-related-hostap-patch-for-2.10-based-hostap.patch \
-	hostap-patches/0002-radius_failover_2.10.patch"
+hostap-patches/0002-radius_failover_2.10.patch \
+	hostap-patches/0003-mbssid_support_2.10.patch"
 echo "Applying patches ..."
 git am $patch_filenames
 
