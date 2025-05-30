c7a93858da0020badbca98e4496cf5566665ec18
sjs889 <Srijeyarankesh_JS@comcast.com>
2025-05-30 05:54:30 +0000

    Repurposed radius debug prints added

--- source/core/wifi_ctrl_webconfig.c
+++ source/core/wifi_ctrl_webconfig.c
@@ -2099,8 +2099,8 @@ static int update_lnf_vap_as_per_hotspot_enabled(wifi_vap_info_t *lnf_vap_info, 
     lnf_vap_info->u.bss_info.security.repurposed_radius = hotspot_vap_info->u.bss_info.security.u.radius;
     wifi_util_info_print(WIFI_SRI, "%s:%d Value of lnf vapInfo->vap_name = %s and lnf_vap_info->radio_index = %d and lnf_vap_info->u.bss_info.mdu_enabled = %d\n", __func__, __LINE__, lnf_vap_info->vap_name, lnf_vap_info->radio_index, lnf_vap_info->u.bss_info.mdu_enabled);
     wifi_util_info_print(WIFI_SRI, "%s:%d Lnf Primary IP = %s Primary Port = %d Secondary Ip = %s Secondary Port = %d\n",
-        __func__, __LINE__, lnf_vap_info->u.bss_info.security.u.radius.ip, lnf_vap_info->u.bss_info.security.u.radius.port,
-        lnf_vap_info->u.bss_info.security.u.radius.s_ip, lnf_vap_info->u.bss_info.security.u.radius.s_port);
+        __func__, __LINE__, lnf_vap_info->u.bss_info.security.repurposed_radius.ip, lnf_vap_info->u.bss_info.security.repurposed_radius.port,
+        lnf_vap_info->u.bss_info.security.repurposed_radius.s_ip, lnf_vap_info->u.bss_info.security.repurposed_radius.s_port);
     //Below condition ok for Hotspot VAP enablement as we only take care of actual config change 
     if (lnf_vap_info->u.bss_info.mdu_enabled && lnf_vap_info->u.bss_info.enabled != hotspot_vap_info->u.bss_info.enabled)
     {
