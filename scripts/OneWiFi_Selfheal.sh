2f2c7035cf1c83c41ba3692a068b01c7811bf4bb
AmaleshNandh29 <amalesh_nandh@comcast.com>
2025-06-25 21:02:50 +0530

    Update self_heal script to run force reset private vap
    Impacted Platforms:
    XB7, XB8, XB10
    
    Reason for change: Update self_heal script to run force reset private vap
    
    Test Procedure: Private vap should be force reset within 5-10 mins after
    webconfig is up if default SSID is set
    
    Risks: Low
    Priority:P1
    
    Signed-off-by:Amalesh_Nandh@comcast.com

--- scripts/OneWiFi_Selfheal.sh
+++ scripts/OneWiFi_Selfheal.sh
@@ -411,17 +411,17 @@ do
         onewifi_restart_wifi
     fi
 
-    if [ $force_reset_subdoc -le  2 ]; then
+    if [ $force_reset_subdoc -le  5 ]; then
         if [ -f  $SW_UPGRADE_DEFAULT_FILE ]; then
             webcfg_rfc_enabled=`dmcli eRT getv Device.X_RDK_WebConfig.RfcEnable | grep "value" | cut -d ':' -f3-5`
             echo_t "webcfg_rfc status is $webcfg_rfc_enabled" >>  /rdklogs/logs/wifi_selfheal.txt
             if [ "$webcfg_rfc_enabled" = "true" ]; then
                 dmcli eRT setv Device.X_RDK_WebConfig.webcfgSubdocForceReset string privatessid
                 echo_t "Selfheal execution to force_reset on private vaps passed from WebConfig" >> /rdklogs/logs/wifi_selfheal.txt
-                ((force_reset_subdoc++))
                 rm -f $SW_UPGRADE_DEFAULT_FILE
             fi
         fi
+        ((force_reset_subdoc++))
     fi
 
     # Check if OneWifi process RSS memory usage exceeds threshold, if does restart OneWifi.
