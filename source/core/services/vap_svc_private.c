ee6a294e0a33845f0b08813095f726441a64db3e
sjs889 <Srijeyarankesh_JS@comcast.com>
2025-07-04 07:45:16 +0000

    Enhance MW

--- source/core/services/vap_svc_private.c
+++ source/core/services/vap_svc_private.c
@@ -84,7 +84,7 @@ static int configure_lnf_psk_radius_from_hotspot(wifi_vap_info_t *vap_info)
                              __FUNCTION__, __LINE__, vap_info->vap_index);
         return -1;
     }
-    memcpy(&vap_info->u.bss_info.security.repurposed_radius, &hotspot_vap_info->u.bss_info.security.u.radius, sizeof(hotspot_vap_info->u.bss_info.security.u.radius));
+    memcpy((unsigned char *)&vap_info->u.bss_info.security.repurposed_radius, (unsigned char *)&hotspot_vap_info->u.bss_info.security.u.radius, sizeof(hotspot_vap_info->u.bss_info.security.u.radius));
     wifi_util_dbg_print(WIFI_CTRL, "%s:%d LNF RADIUS Config for vap name = %s - Primary IP: %s Port: %d, Secondary IP: %s Port: %d\n",
                        __func__, __LINE__, vap_info->vap_name,
                        vap_info->u.bss_info.security.repurposed_radius.ip,
@@ -160,14 +160,15 @@ int vap_svc_private_update(vap_svc_t *svc, unsigned int radio_index, wifi_vap_in
             update_lnf_psk_vap_hal_prop_bridge_name(svc, p_tgt_vap_map);
             // Since DB persistence not supported as of now for MDU LnF, copying it to here. To be moved in future.
             wifi_vap_info_t* lnf_vap_info = NULL;
-            lnf_vap_info = (wifi_vap_info_t *)get_wifidb_vap_parameters(vap_info->vap_index);
+            lnf_vap_info = (wifi_vap_info_t *)get_wifidb_vap_parameters(p_tgt_vap_map->vap_array[0].vap_index);
             if (lnf_vap_info == NULL) {
+                wifi_util_error_print(WIFI_CTRL,"%s:%d LnF VAP info not found for vap_index=%d\n",__FUNCTION__,__LINE__,map->vap_array[i].vap_index);
                 return -1;
             }
             if (lnf_vap_info->u.bss_info.mdu_enabled) {
-                memcpy(&lnf_vap_info->u.bss_info.security.repurposed_radius, &p_tgt_vap_map->vap_array[0].u.bss_info.security.repurposed_radius, sizeof(lnf_vap_info->u.bss_info.security.repurposed_radius));
+                memcpy((unsigned char *)&lnf_vap_info->u.bss_info.security.repurposed_radius, (unsigned char *)&p_tgt_vap_map->vap_array[0].u.bss_info.security.repurposed_radius, sizeof(lnf_vap_info->u.bss_info.security.repurposed_radius));
             } else {
-                lnf_vap_info->u.bss_info.security.repurposed_radius = 0;
+                memset((unsigned char *)&lnf_vap_info->u.bss_info.security.repurposed_radius, 0, sizeof(lnf_vap_info->u.bss_info.security.repurposed_radius));
             }
         }
         memcpy((unsigned char *)&map->vap_array[i], (unsigned char *)&p_tgt_vap_map->vap_array[0],
