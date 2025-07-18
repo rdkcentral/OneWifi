c03056694f8e4a4befc73dbc30f069de96ff527d
SanjayVenkatesan <sanjayvenkatesan1902@gmail.com>
2025-07-18 19:23:21 +0530

    Update wifi_db_apis.c

--- source/db/wifi_db_apis.c
+++ source/db/wifi_db_apis.c
@@ -83,7 +83,6 @@
 #define ONEWIFI_DB_VERSION_RSS_MEMORY_THRESHOLD_FLAG 100035
 #define ONEWIFI_DB_VERSION_MGT_FRAME_RATE_LIMIT 100036
 
-
 ovsdb_table_t table_Wifi_Radio_Config;
 ovsdb_table_t table_Wifi_VAP_Config;
 ovsdb_table_t table_Wifi_Security_Config;
