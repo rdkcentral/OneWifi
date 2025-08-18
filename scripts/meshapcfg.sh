a3ebb717dff9b785f8ec55688ae1a0dffe7cb113
Pavlo Uvarov <Pavlo.Uvarov@gmail.com>
2025-08-18 16:31:56 +0300

    RDKB-59707: Get rid of TG1 string literals
    Reason for change: Removind TG1 string literals according with OneWifi audit.
    
    Test procedure: -
    
    Risks: Low
    
    Priority: P1

--- scripts/meshapcfg.sh
+++ scripts/meshapcfg.sh
@@ -25,13 +25,14 @@ MODEL_NUM=`grep MODEL_NUM /etc/device.properties | cut -d "=" -f2`
 qca_cfg=0
 DPC394="DPC3941"
 DPC393="DPC3939"
+TG1="TG1682G"
 if [ "$BOX_TYPE" = "XB3" ]; then
 sycfgfile="/nvram/syscfg.db"
 else
 sycfgfile="/opt/secure/data/syscfg.db"
 fi
 
-if [ $MODEL_NUM == "$DPC394" ] || [ $MODEL_NUM == "TG1682G" ]  || [ $MODEL_NUM == "$DPC393" ]; then
+if [ $MODEL_NUM == "$DPC394" ] || [ $MODEL_NUM == "$TG1" ]  || [ $MODEL_NUM == "$DPC393" ]; then
  # RDKB-15951: Create a bridge for Mesh Bhaul and add vlan to it
  echo "Creating Mesh Bhaul bridge"
  brctl addbr br403
