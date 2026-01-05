9f9b52324b87dbb053cccf71e5db2f4e9a348e99
MG-kildon <MARIAGNANAKILDON_S@comcast.com>
2026-01-05 14:36:19 +0530

    DTMESH-744: Setting Beacon interval to 100 (#813)
    * setting beacon interval to 100
    * Setting beacon interval as 100 for 5g radio

--- source/db/wifi_db_apis.c
+++ source/db/wifi_db_apis.c
@@ -6840,14 +6840,14 @@ int wifidb_init_radio_config_default(int radio_index,wifi_radio_operationParam_t
 #endif /* defined(CONFIG_IEEE80211BE) && defined(_PLATFORM_BANANAPI_R4_) */
 
 #if defined (_PP203X_PRODUCT_REQ_) || defined (_GREXT02ACTS_PRODUCT_REQ_)
-            cfg.beaconInterval = 200;
+            cfg.beaconInterval = 100;
 #endif
             break;
         case WIFI_FREQUENCY_5_BAND:
         case WIFI_FREQUENCY_5L_BAND:
             cfg.operatingClass = 128;
 #if defined (_PP203X_PRODUCT_REQ_) || defined (_GREXT02ACTS_PRODUCT_REQ_)
-            cfg.beaconInterval = 200;
+            cfg.beaconInterval = 100;
 #endif
             if (ctrl->network_mode == rdk_dev_mode_type_em_node)
                 cfg.channel = 36;
