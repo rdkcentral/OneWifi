e63f9339eb5ee2abc1a3678a2f4ab69110d73df8
poornakumar-pk <poornakumar_mezhiselvam@comcast.com>
2026-02-18 18:36:28 +0530

    RDKB-63533 5G OCS not operational when GW uses a DFS channel
    Reason for change:  5G OCS shoud be operational when GW uses a DFS channel
    Test Procedure: Change the channel to dfs and check for ocs scan
    Risks: P0
    Signed-off-by: poornakumar_mezhiselvam@comcast.com

--- source/stats/wifi_stats_radio_channel.c
+++ source/stats/wifi_stats_radio_channel.c
@@ -957,19 +957,7 @@ int execute_radio_channel_api(wifi_mon_collector_element_t *c_elem, wifi_monitor
         if (args->channel_list.num_channels == 0) {
             return RETURN_ERR;
         }
-        // dont run offchan scan if device current using dfs channel
-        if (radioOperation->band == WIFI_FREQUENCY_5L_BAND ||
-            radioOperation->band == WIFI_FREQUENCY_5H_BAND ||
-            radioOperation->band == WIFI_FREQUENCY_5_BAND) {
-            if (is_5g_20M_channel_in_dfs(radioOperation->channel) ||
-                radioOperation->channelWidth == WIFI_CHANNELBANDWIDTH_160MHZ) {
-                wifi_util_dbg_print(WIFI_MON,
-                    "%s:%d  off channel scan not executed duo to DFS channel in use for radio "
-                    "index %d\n",
-                    __func__, __LINE__, args->radio_index);
-                return RETURN_OK;
-            }
-        }
+
         // Fill on-channel scan list
         if (get_on_channel_scan_list(radioOperation->band, radioOperation->channelWidth,
 		radioOperation->channel, on_chan_list, &onchan_num_channels) != 0) {
