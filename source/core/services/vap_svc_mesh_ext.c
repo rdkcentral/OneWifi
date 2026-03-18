833bab7587b93d53cd40aad22d547bae99658ae5
Dharmalakshmi Annamalai <Dharmalakshmi_Annamalai@comcast.com>
2026-03-18 09:58:00 +0000

    testing

--- source/core/services/vap_svc_mesh_ext.c
+++ source/core/services/vap_svc_mesh_ext.c
@@ -239,8 +239,16 @@ int sort_bss_results_by_ranking(bss_candidate_t *scan_list, int count)
     for (int i = 0; i < valid_count; i++) {
         float snr_diff = 0.0;
         if (scores[i].bucket == 2) {
+            int radio_index = 0;
+            if (convert_freq_band_to_radio_index(scan_list[i].external_ap.oper_freq_band, &radio_index) == RETURN_ERR) {
+                wifi_util_error_print(WIFI_CTRL, "%s:%d: Failed to get radio index for band %d\n",
+                 __func__, __LINE__, scan_list[i].external_ap.oper_freq_band);
+                free(scores);
+                return RETURN_ERR;
+            }
+            wifi_util_dbg_print(WIFI_CTRL, "%s:%d Scan-count : %d Ignite Threshold Values [ %s %f %f %f %f]\n", __func__, __LINE__, count, mgr->ignite_config[radio_index].ignite_name, mgr->ignite_config[radio_index].min_chanutil_threshold ,mgr->ignite_config[radio_index].max_chanutil_threshold ,mgr->ignite_config[radio_index].SNR_threshold ,mgr->ignite_config[radio_index].SNR_difference);
              wifi_util_dbg_print(WIFI_CTRL, "[%s %d] BSSID : %s SNR : %f\n", __func__, __LINE__, to_mac_str(scores[i].candidate->external_ap.bssid, bssid_str), scores[i].candidate->external_ap.snr);
-             ignite_config_t *cfg = &mgr->ignite_config[scores[i].radio_index];
+             ignite_config_t *cfg = &mgr->ignite_config[radio_index];
 	     snr_diff = scores[i].candidate->external_ap.snr - max_bucket1_snr;
 	     wifi_util_dbg_print(WIFI_CTRL, "[%s %d] ignite-snr-diff : %f snr-diff : %f\n", __func__, __LINE__, cfg->SNR_difference, snr_diff);
              if (snr_diff > cfg->SNR_difference) {
