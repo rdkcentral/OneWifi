236cd9f8f3da8fcf6beefa787a803f3ce19e2109
HarshavardhanP1 <Harshavardhan_Pulluru@comcast.com>
2026-04-07 14:46:56 +0530

    RDKB-63321,RDKB-63819 : XfinityWiFi Telemetry Enhancements (#948) (#1038)
    * RDKB-63321,RDKB-63819 : XfinityWiFi Telemetry Enhancements (#948)
    
    * RDKB-63321,RDKB-63156 : XfinityWiFi Telemetry Enhancements
    * REmoval of vlan_id changes from hal
    
    ---------
    
    Co-authored-by: Rajagopalaswamy M <Rajagopalaswamy_Muthukrishnan@comcast.com>
    Co-authored-by: Raja Shah <167130396+rshah-1@users.noreply.github.com>
    Co-authored-by: Sathish Kumar Gnanasekaran <gsathish86@gmail.com>
    
    * refactor the format
    
    * refactor
    
    * refactoring refactor
    
    ---------
    
    Co-authored-by: Rajagopalaswamy M <Rajagopalaswamy_Muthukrishnan@comcast.com>
    Co-authored-by: Raja Shah <167130396+rshah-1@users.noreply.github.com>
    Co-authored-by: Sathish Kumar Gnanasekaran <gsathish86@gmail.com>

--- include/wifi_events.h
+++ include/wifi_events.h
@@ -164,6 +164,7 @@ typedef enum {
     wifi_event_type_rsn_override_rfc,
     wifi_event_type_sta_client_info,
     wifi_event_type_start_sta_channel_scan,
+    wifi_event_type_xfi_tel_enable_rfc,
     wifi_event_command_max,
 
     wifi_event_monitor_diagnostics = wifi_event_type_base
@@ -200,6 +201,7 @@ typedef enum {
     wifi_event_monitor_auth_req,
     wifi_event_monitor_update_interop_interval,
     wifi_event_monitor_channel_status,
+    wifi_event_monitor_eap_status,
     wifi_event_monitor_max,
 
     // Tunnel
