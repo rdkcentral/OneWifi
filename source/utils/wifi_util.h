abac594234de53b269afd4a1b64f8c9524861a82
HollowMax <maxholovatyi5@gmail.com>
2026-05-29 09:16:53 +0300

    RDKB-64763: Support of MLO VIFs and clients in OVSM
    Reason for change: Add utility function
    Test Procedure: 1. Check if fields mld_addr and mld_if_name are preset
    in the Wifi_VIF_State table.
        2. Connect mlo capable client.
        3. Cehck the fields mac and mld_addr in the
           Wifi_Associated_Clients_Table
    
    Risks: Medium
    Priority: P1
    
    Signed-off-by: HollowMax <maxholovatyi5@gmail.com>

--- source/utils/wifi_util.h
+++ source/utils/wifi_util.h
@@ -479,6 +479,7 @@ int mac_address_from_name(const char *ifname, mac_address_t mac);
 bool is_zero_mac(const uint8_t *mac);
 bool is_valid_encr_for_mode(wifi_security_modes_t mode, wifi_encryption_method_t encr);
 int get_mesh_sta_mac_address_for_radio(wifi_platform_property_t *wifi_prop, unsigned int radio_index, mac_address_t mac);
+bool get_mlo_vap_name_from_per_radio(const char *vap_name, char *out, size_t out_size);
 #ifdef __cplusplus
 }
 #endif
