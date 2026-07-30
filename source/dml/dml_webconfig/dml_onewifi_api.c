587d7bcb8dfc4daf39acca053ddbf797d84513fb
Mateusz Cieslak <mateusz.cieslak2@globallogic.com>
2026-07-30 15:59:17 +0200

    Formatting cleanup

--- source/dml/dml_webconfig/dml_onewifi_api.c
+++ source/dml/dml_webconfig/dml_onewifi_api.c
@@ -2061,8 +2061,8 @@ bool wifi_factory_reset(bool factory_reset_all_vaps)
         wifidb_init_radio_config_default(i,rcfg,&fcfg);
 
         wifi_rfc_dml_parameters_t *rfc_param = get_wifi_db_rfc_parameters();
-        if ((rfc_param == NULL) || wifidb_get_rfc_config(0,rfc_param) != 0) {
-            wifi_util_error_print(WIFI_DMCLI,"%s:%d: Error getting RFC config\n",__func__, __LINE__);
+        if ((rfc_param == NULL) || wifidb_get_rfc_config(0, rfc_param) != 0) {
+            wifi_util_error_print(WIFI_DMCLI,"%s:%d: Error getting RFC config\n", __func__, __LINE__);
 			goto cleanup;
         }
 
