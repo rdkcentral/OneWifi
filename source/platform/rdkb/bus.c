dcbb65ecf133d2584162eba85ab08f551536503c
Mateusz Cieslak <mateusz.cieslak2@globallogic.com>
2026-08-05 02:16:33 +0200

    Fix indentation per copilot

--- source/platform/rdkb/bus.c
+++ source/platform/rdkb/bus.c
@@ -562,11 +562,11 @@ bus_error_t set_rbus_property_data(char *event_name, rbusProperty_t property, ra
         case bus_data_type_uint32:
             rbusValue_SetUInt32(value, bus_data->raw_data.u32);
         break;
-		case bus_data_type_uint8:
-		    rbusValue_SetUInt8(value, bus_data->raw_data.u8);
-		break;
-		case bus_data_type_uint16:
-		    rbusValue_SetUInt16(value, bus_data->raw_data.u16);
+        case bus_data_type_uint8:
+            rbusValue_SetUInt8(value, bus_data->raw_data.u8);
+        break;
+        case bus_data_type_uint16:
+            rbusValue_SetUInt16(value, bus_data->raw_data.u16);
 		break;
         case bus_data_type_int32:
             rbusValue_SetInt32(value, bus_data->raw_data.i32);
