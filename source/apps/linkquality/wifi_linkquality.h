e8d4b5733aead07325ef2aa51a6163f74db825a1
sanjay-8610 <Sanjay_Vijayabaskaran@comcast.com>
2026-09-03 16:08:29 +0000

    RDKB-66764, RDKB-66765, RDKB-66766 - Wi-Fi Experience Index feature.
    LQ, SC and GC pillars added.
    
    Reason for change: Implemented WEI feature
    Test Procedure: Connect multiple clients and verify the Scoring and Measurement bundles
    
    Risks: Moderate
    
    Signed-off-by: Sanjay_Vijayabaskaran@comcast.com

--- source/apps/linkquality/wifi_linkquality.h
+++ source/apps/linkquality/wifi_linkquality.h
@@ -28,7 +28,15 @@ extern "C" {
 #include "run_qmgr.h"
 #include "wifi_base.h"
 #include "wifi_webconfig.h"
+#include "wifi_hal.h"
+#include "wifi_linkquality_libs.h"
 
+#define MAX_STR_LEN_LQ 128
+#define IGNITE_SCORE_LOG_INTERVAL_MS 900000 // 15 mins
+#define IGNITE_INITIAL_PUBLISH_ITERATIONS 5
+
+
+#define MAC_ADDRESS_LEN 6
 typedef struct {
     double last_score;
     double last_threshold;
@@ -44,6 +52,11 @@ typedef struct {
     int size;
     ignite_lq_state_t ignite;
 } linkquality_data_t;
+
+typedef uint8_t mac_address_t[MAC_ADDRESS_LEN];
+
+#define CTRL_CAP_SZ 8
+
 
 #ifdef __cplusplus
 }
