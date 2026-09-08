a92fcbbc637d65c467e7440a94d4d5eac720d3ad
Vitaliy Lyashenko <v.for.vitalik@gmail.com>
2026-09-08 17:53:32 +0300

    XB10-3059: add reset of table_de_stamld_index of deleted APMLD
    Reason for change: Add reset of related table_de_stamld_index
    in case of APMLD is removed. Remove of APMLD also deletes all
    child nodes/lists so we need to reset tracking counters aswell
    
    Test Procedure:
    1. Enable MLO & configure MLD
    2. Connect MLO device to configured MLD
    3. Check if STAMLD instance is present in
    Device.WiFi.DataElements.Network.Device.1.APMLD.*.STAMLD.*.
    4. Disable MLD e.g.
    dmcli eRT setv Device.WiFi.SSID.1.MLDUnit int -1
    dmcli eRT setv Device.WiFi.SSID.2.MLDUnit int -1
    dmcli eRT setv Device.WiFi.SSID.17.MLDUnit int -1
    dmcli eRT setv Device.WiFi.ApplyAccessPointSettings bool true
    5. Reconnect the client
    6. Check that APMLD & STAMLD is empty with
    Device.WiFi.DataElements.Network.Device.1.APMLD.*.STAMLD.*.
    7. Re-enable MLD
    dmcli eRT setv Device.WiFi.SSID.1.MLDUnit int 0
    dmcli eRT setv Device.WiFi.SSID.2.MLDUnit int 0
    dmcli eRT setv Device.WiFi.SSID.17.MLDUnit int 0
    dmcli eRT setv Device.WiFi.ApplyAccessPointSettings bool true
    8. reconnect the MLO client and check if APMLD & STAMLD are
    displayed correctly
    
    Priority: P1
    Risks: Low
    
    Signed-off-by: Vitaliy Lyashenko <v.for.vitalik@gmail.com>

--- source/platform/common/data_model/wfa/wfa_data_model.c
+++ source/platform/common/data_model/wfa/wfa_data_model.c
@@ -219,6 +219,19 @@ bus_error_t de_apmld_sync_handler(char const* tableName, bus_data_prop_t *inPara
     wifi_util_dbg_print(WIFI_DMCLI,"%s:%d enter %s, numrow %d, numapmld %d\r\n", __func__, __LINE__, tableName, num_row, num_apmld);
 
     de_sync_rows(tableName, num_row, num_apmld, &p_dml_param->table_de_apmld_index, false);
+    if (p_dml_param->table_de_apmld_index > MLD_UNIT_COUNT) {
+        wifi_util_error_print(WIFI_DMCLI,"%s:%d invalid APMLD index\r\n", __func__, __LINE__);
+        return bus_error_general;
+    }
+
+    if (num_row != num_apmld) {
+        num_row = (num_row > MLD_UNIT_COUNT) ? MLD_UNIT_COUNT : num_row;
+        //reset stamld indexes for APMLD groups which were removed (unregistered)
+        for (uint32_t i = 0; i < num_row; i++) {
+            p_dml_param->table_de_stamld_index[i] = 0;
+        }
+    }
+
     return bus_error_success;
 }
 
