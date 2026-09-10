8283fb05b5e4b872742b114ee7741d341992d471
Raja Shah <167130396+rshah-1@users.noreply.github.com>
2026-09-10 12:31:05 -0700

    Merge pull request #1420 from pramod7456/wei_rfc_review
    RDKB-66764, RDKB-66765, RDKB-66766 - Wi-Fi Experience Index feature.

--- source/core/wifi_ctrl.c
+++ source/core/wifi_ctrl.c
@@ -2968,8 +2968,11 @@ wei_rfc_dml_parameters_t *get_ctrl_wei_rfc_parameters(void)
     wifi_ctrl_t *ctrl = (wifi_ctrl_t *)get_wifictrl_obj();
     wifi_mgr_t *g_wifi_mgr = get_wifimgr_obj();
 
+    pthread_mutex_lock(&g_wifi_mgr->data_cache_lock);
     memcpy(&ctrl->wei_rfc_params, &g_wifi_mgr->wei_rfc_dml_parameters,
         sizeof(wei_rfc_dml_parameters_t));
+    pthread_mutex_unlock(&g_wifi_mgr->data_cache_lock);
+
     return &ctrl->wei_rfc_params;
 }
 
@@ -3022,8 +3025,6 @@ wifi_rfc_dml_parameters_t *get_ctrl_rfc_parameters(void)
         g_wifi_mgr->rfc_dml_parameters.tcm_secure_6g_rfc;
     g_wifi_mgr->ctrl.rfc_params.wpa3_compatibility_enable =
         g_wifi_mgr->rfc_dml_parameters.wpa3_compatibility_enable;
-    g_wifi_mgr->ctrl.rfc_params.wei_rfc_mask =
-        g_wifi_mgr->rfc_dml_parameters.wei_rfc_mask;
     g_wifi_mgr->ctrl.rfc_params.xfi_tel_enable_rfc =
         g_wifi_mgr->rfc_dml_parameters.xfi_tel_enable_rfc;
     g_wifi_mgr->ctrl.rfc_params.multiap_rfc =
