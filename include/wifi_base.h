c7ae7fbf92c05864dc75ea2579ee9536dd2f7f69
Brayan Milczarek <brayan_milczarek@comcast.com>
2026-09-11 07:26:37 +0200

    RDKB-66065:  RFC MLO Enable (#1315) (#1409)
    Reason for change: Send notification to telemetry whenever MLO is
    completely disabled on device.
    Test Procedure: Enable/disable MLO, check if appropriate messages are
    being sent.
    Risks: Low
    Priority: P1
    
    Signed-off-by: Brayan Milczarek <brayan_milczarek@comcast.com>
    Co-authored-by: Stanislav Kuchar <stanislav.kuchar@gmail.com>
    Co-authored-by: Brayan Milczarek <brayan.milczarek@comcast.com>
    Co-authored-by: Narendra Varma Dandu <narendandu@gmail.com>

--- include/wifi_base.h
+++ include/wifi_base.h
@@ -92,6 +92,12 @@ extern "C" {
 #define WIFI_LINK_QUALITY_DATA      "Device.WiFi.LinkQualityData"
 #define WIFI_LINK_QUALITY_FLAGS     "Device.WiFi.LinkQualityFlags"
 #define WIFI_IGNITE_STATUS "Device.WiFi.EndPoint.1.LinkQualityStatus"
+#define WIFI_NASTA_RESPONSE_EVENT                      "Device.WiFi.EM.NaStaResponse"
+#define WIFI_ACCESSPOINT_GET_NASTA                     "Device.WiFi.AccessPoint.{i}.X_RDKCENTRAL-COM_GetNaSta"
+#define WIFI_NETWORKDEVICESSTATUS_MLORFCENABLE \
+    "Device.DeviceInfo.X_RDKCENTRAL-COM_Report.NetworkDevicesStatus.MloRfcEnable"
+#define WIFI_INTERFACEDEVICESWIFI_MLORFCENABLE \
+    "Device.DeviceInfo.X_RDKCENTRAL-COM_Report.InterfaceDevicesWifi.MloRfcEnable"
 
 #ifndef MAX_NUM_MLD_LINKS
 #define MAX_NUM_MLD_LINKS 15
--- source/core/wifi_ctrl.h
+++ source/core/wifi_ctrl.h
@@ -436,6 +436,7 @@ void hotspot_timing_stop(void);
 #if defined(CONFIG_IEEE80211BE) && !defined(CONFIG_GENERIC_MLO)
 void update_mld_groups(webconfig_subdoc_decoded_data_t *data, char **vap_names,
     unsigned int vap_names_size, wifi_dbg_type_t log_type);
+void update_mlo_rfc_enable(bool init);
 #endif /* CONFIG_IEEE80211BE && !CONFIG_GENERIC_MLO */
 
 wifi_vap_info_t *get_mlo_partner_link_by_link_id(wifi_vap_info_t *vapInfo, UINT link_id);
