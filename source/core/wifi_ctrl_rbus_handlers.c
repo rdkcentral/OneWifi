2839c917348e0985114f7b411754f2c9d0bc00fd
Yashodha KB <yakshagowda37@gmail.com>
2026-09-18 21:16:15 +0530

    RDKBDEV-3515: Handle memory allocation failure

--- source/core/wifi_ctrl_rbus_handlers.c
+++ source/core/wifi_ctrl_rbus_handlers.c
@@ -1312,6 +1312,11 @@ bus_error_t publish_endpoint_status(wifi_ctrl_t *ctrl, int connection_status)
     memset(&data, 0, sizeof(raw_data_t));
     data.data_type = bus_data_type_string;
     data.raw_data.bytes = malloc(MAX_STATUS_LEN);
+    if (data.raw_data.bytes == NULL) {
+        wifi_util_error_print(WIFI_CTRL, "%s:%d: Failed to allocate memory\n",
+                          __func__, __LINE__);
+        return bus_error_out_of_resources;
+    }
     data.raw_data_len = MAX_STATUS_LEN;
     memset(data.raw_data.bytes, '\0', MAX_STATUS_LEN);
     if (connection_status == 2) { // connected state
